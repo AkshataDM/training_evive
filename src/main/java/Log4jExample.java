c93d38a44b82068c39d7525eb18dc4baa90e7041
AkshataDM <oxta28@gmail.com>
2015-10-25 11:14:49 +0530

    Initial commit

---
+++ src/main/java/Log4jExample.java
@@ -0,0 +1,13 @@
+import java.util.logging.Logger;
+
+/**
+ * Created by akshata on 25/10/15.
+ */
+public class Log4jExample {
+
+    static Logger log = Logger.getLogger(Log4jExample.class.getName());
+
+    public static void main(String[] args) {
+        System.out.println("Log4j not working currently");
+    }
+}
